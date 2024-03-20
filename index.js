2a5e775e4d7c2f01dbcd42d9d2e302abfbb8f499
Jannis R <mail@jannisr.de>
2024-03-20 22:15:43 +0100

    TripUpdates matching: expose metrics

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 import {connectToPostgres} from './lib/db.js'
 import {createLogger} from './lib/logger.js'
+import {
+	register as metricsRegister,
+	createMetricsServer,
+} from './lib/metrics.js'
 import {createMatchTripUpdate} from './lib/match-trip-update.js'
 
 // todo: get ride of this untestable singleton
 const db = await connectToPostgres()
 
+const metricsServer = createMetricsServer()
+await metricsServer.start()
+
 const {matchTripUpdate} = createMatchTripUpdate({
 	db,
 	logger: createLogger('match-trip-update', process.env.LOG_LEVEL_MATCHING || 'error'),
+	metricsRegister,
 })
 
 const stopMatching = async () => {
 	await db.end()
+	metricsServer.close()
 }
 
 export {
