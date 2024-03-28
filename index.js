d2018eb5afd80edc9dd4f1ee6e8a5f34e1337132
Jannis R <mail@jannisr.de>
2024-03-28 23:19:39 +0100

    basic gtfs import

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ import {
 	parseEncodedFeed,
 	createParseAndProcessFeed,
 } from './lib/match.js'
+import {
+	startRefreshingNyctSubwayScheduleFeed,
+} from './lib/refresh-schedule-feeds.js'
 
 export {
 	parseEncodedFeed,
 	createParseAndProcessFeed,
+	startRefreshingNyctSubwayScheduleFeed,
 }
