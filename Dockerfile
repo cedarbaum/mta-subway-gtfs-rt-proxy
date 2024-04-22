31ae26a7b54e26c659227e266bc1ec734ccffb0d
Jannis R <mail@jannisr.de>
2024-04-23 00:42:36 +0200

    add start script

--- Dockerfile
+++ Dockerfile
@@ -64,4 +64,4 @@ EXPOSE 3000
 
 ENV PORT 3000
 
-CMD ["node", "index.js"]
+CMD ["node", "start.js"]
---
+++ start.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import {createService} from './index.js'
+
+await createService()
