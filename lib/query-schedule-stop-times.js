cedarbaum/mta-subway-gtfs-rt-proxy

commit 26d42634ae5148d344d8a97dc8f0fc6886095d4a

match & extend VehiclePositions ✅

lib/query-schedule-stop-times.js
@@ -10,10 +10,12 @@ const queryScheduleStopTimes = async (cfg) => {
 		route_id,
 		start_date,
 		trip_id,
+		stop_id = null,
 		db,
 		matchingSuccesses,
 		matchingFailures,
 		dbQueryTimeSeconds,
+		limit,
 	} = cfg
 	ok(route_id, 'missing/empty route_id')
 	ok(start_date, 'missing/empty start_date')
@@ -55,10 +57,11 @@ const queryScheduleStopTimes = async (cfg) => {
 		].join('')
 
 		// todo: this format isn't used in the GTFS Schedule data (anymore?)
+		// todo: DRY with query below
 // 		const t0 = performance.now()
 // 		const {rows: scheduleStopTimes} = await db.query({
 // 			// allow `pg` to create a prepared statement
-// 			name: 'stop_times_exact',
+// 			name: `stop_times_suffix${stop_id === null ? '' : '_stop_id'}`,
 // 			text: `\
 // 				SELECT
 // 					trip_id,
@@ -67,16 +70,22 @@ const queryScheduleStopTimes = async (cfg) => {
 // 					t_arrival, t_departure
 // 				FROM arrivals_departures
 // 				WHERE route_id = $1
+// 				-- Because we identify our parameters using their index in the "values" array below, dynamically inserting the entire "stop_id" condition would mean that all subsequent parameters' indices change, making the code much more complex.
+// 				-- Instead, if "stop_id" is "null", we make this condition *always* true by comparing "$5" to itself. Because in SQL "NULL" is compared using the "is" operator, we insert "1" below, yielding "AND 1 = 1".
+// 				AND ${stop_id === null ? '$5' : 'stop_id'} = $5
 // 				AND "date" = $2
 // 				AND trip_id = $3
 // 				-- todo: for now, we don't support frequencies.txt-based trips yet
 // 				AND frequencies_it = -1
 // 				ORDER BY stop_sequence_consec ASC
+// 				LIMIT $4
 // `,
 // 			values: [
 // 				route_id,
 // 				isoStartDate,
 // 				_trip_id,
+// 				limit,
+// 				stop_id === null ? 1 : stop_id,
 // 			],
 // 		})
 // 		dbQueryTimeSeconds.observe({
@@ -97,7 +106,7 @@ const queryScheduleStopTimes = async (cfg) => {
 		const t0 = performance.now()
 		const {rows: scheduleStopTimes} = await db.query({
 			// allow `pg` to create a prepared statement
-			name: 'stop_times_suffix',
+			name: `stop_times_suffix${stop_id === null ? '' : '_stop_id'}`,
 			text: `\
 				SELECT
 					trip_id,
@@ -106,11 +115,15 @@ const queryScheduleStopTimes = async (cfg) => {
 					t_arrival, t_departure
 				FROM arrivals_departures
 				WHERE route_id = $1
+				-- Because we identify our parameters using their index in the "values" array below, dynamically inserting the entire "stop_id" condition would mean that all subsequent parameters' indices change, making the code much more complex.
+				-- Instead, if "stop_id" is "null", we make this condition *always* true by comparing "$5" to itself. Because in SQL "NULL" is compared using the "is" operator, we insert "1" below, yielding "AND 1 = 1".
+				AND ${stop_id === null ? '$5' : 'stop_id'} = $5
 				AND "date" = $2
 				AND trip_id LIKE $3
 				-- todo: for now, we don't support frequencies.txt-based trips yet
 				AND frequencies_it = -1
 				ORDER BY stop_sequence_consec ASC
+				LIMIT $4
 `,
 			values: [
 				route_id,
@@ -121,6 +134,8 @@ const queryScheduleStopTimes = async (cfg) => {
 				// Note: We assume that the GTFS Realtime trip ID uniquely identifies the trip within the route & date or, put in another way, that no two trips of the same route & date share the same trip ID suffix.
 				// > For example, if a trip_id in trips.txt is A20111204SAT_021150_2..N08R, the GTFS-realtime trip_id will be 021150_2..N08R which is unique within the day type (WKD, SAT, SUN).
 				`%_${trip_id}`,
+				limit,
+				stop_id === null ? 1 : stop_id,
 			],
 		})
 		dbQueryTimeSeconds.observe({

index.js
@@ -5,6 +5,9 @@ import {
 	createMetricsServer,
 } from './lib/metrics.js'
 import {createMatchTripUpdate} from './lib/match-trip-update.js'
+import {createMatchVehiclePosition} from './lib/match-vehicle-position.js'
+
+const MATCHING_LOG_LEVEL = process.env.LOG_LEVEL_MATCHING || 'error'
 
 // todo: get ride of this untestable singleton
 const db = await connectToPostgres()
@@ -14,7 +17,12 @@ await metricsServer.start()
 
 const {matchTripUpdate} = createMatchTripUpdate({
 	db,
-	logger: createLogger('match-trip-update', process.env.LOG_LEVEL_MATCHING || 'error'),
+	logger: createLogger('match-trip-update', MATCHING_LOG_LEVEL),
+	metricsRegister,
+})
+const {matchVehiclePosition} = createMatchVehiclePosition({
+	db,
+	logger: createLogger('match-vehicle-position', MATCHING_LOG_LEVEL),
 	metricsRegister,
 })
 
@@ -25,5 +33,6 @@ const stopMatching = async () => {
 
 export {
 	matchTripUpdate,
+	matchVehiclePosition,
 	stopMatching, // todo: design a better API
 }

lib/match-trip-update.js
@@ -104,6 +104,7 @@ const createMatchTripUpdate = (cfg) => {
 			matchingSuccesses,
 			matchingFailures,
 			dbQueryTimeSeconds,
+			limit: 1000,
 		})
 
 		if (scheduleStopTimes.length === 0) {

lib/match-vehicle-position.js
@@ -1,0 +1,118 @@
+import {Counter, Summary} from 'prom-client'
+import gtfsRtBindings from './mta-gtfs-realtime.pb.js'
+import {queryScheduleStopTimes} from './query-schedule-stop-times.js'
+
+const {ScheduleRelationship} = gtfsRtBindings.transit_realtime.TripDescriptor
+
+const createMatchVehiclePosition = (cfg) => {
+	const {
+		db,
+		logger,
+		metricsRegister,
+	} = cfg
+
+	const dbQueryTimeSeconds = new Summary({
+		name: 'vehiclepositions_matching_db_query_time_seconds',
+		help: 'when matching VehiclePositions, for how long GTFS Schedule stop_times are queried from the database',
+		registers: [metricsRegister],
+		labelNames: ['route_id', 'by_suffix', 'success'],
+	})
+	const matchingSuccesses = new Counter({
+		name: 'vehiclepositions_matching_successes_total',
+		help: 'nr. of successfully matched VehiclePositions',
+		registers: [metricsRegister],
+		labelNames: ['route_id', 'by_suffix'],
+	})
+	const matchingFailures = new Counter({
+		name: 'vehiclepositions_matching_failures_total',
+		help: 'nr. of successfully matched VehiclePositions',
+		registers: [metricsRegister],
+		labelNames: ['route_id', 'by_suffix'],
+	})
+
+	// Note: This function mutates `vehiclePosition`.
+	const matchVehiclePosition = async (vehiclePosition) => {
+		const {
+			route_id,
+			start_date: startDate,
+			trip_id: realtimeTripId,
+		} = vehiclePosition.trip
+		const {
+			stop_id,
+			current_stop_sequence,
+		} = vehiclePosition
+		const nyctTripDescriptor = vehiclePosition.trip['.nyct_trip_descriptor'] || null
+
+		const logCtx = {
+			routeId: route_id,
+			stopId: stop_id,
+			startDate,
+			realtimeTripId,
+		}
+		logger.trace({
+			...logCtx,
+			vehiclePosition,
+		}, 'matching VehiclePosition')
+
+		const scheduleStopTimes = await queryScheduleStopTimes({
+			route_id,
+			start_date: startDate,
+			trip_id: realtimeTripId,
+			// Note: This assumes that no trip visits a stop more than once.
+			// todo: find a way to handle this, e.g. using stop_sequence
+			stop_id,
+			db,
+			matchingSuccesses,
+			matchingFailures,
+			dbQueryTimeSeconds,
+			limit: 1, // todo: what if there is more than one match?
+		})
+
+		if (scheduleStopTimes.length === 0) {
+			logger.warn(logCtx, 'failed to find matching schedule trip for VehiclePosition')
+			// todo: if trip is duplicated/added, provide TripProperties.{trip_id,start_date,start_time,shape_id}?
+			return null
+		}
+
+		const scheduleStopTime = scheduleStopTimes[0]
+		logCtx.tripId = scheduleStopTime.trip_id
+		logger.debug(logCtx, 'found matching schedule trip for VehiclePosition')
+
+		// We want to expose trip IDs matching the GTFS Schedule data.
+		vehiclePosition.trip.trip_id = scheduleStopTime.trip_id
+
+		vehiclePosition.trip.schedule_relationship = ScheduleRelationship.SCHEDULED
+
+		if (scheduleStopTime.stop_sequence !== current_stop_sequence) {
+			logger.warn({
+				...logCtx,
+				realtimeCurrentStopSequence: current_stop_sequence,
+				stopSequence: scheduleStopTime.stop_sequence,
+			}, 'stop_sequences not matching')
+			// we probably have the wrong stop_times row
+			return null
+		}
+
+		// > When the trip_id corresponds to a non-frequency-based trip, this field should either be omitted or be equal to the value in the GTFS feed. When the trip_id correponds to a frequency-based trip defined in GTFS frequencies.txt, start_time is required and must be specified for trip updates and vehicle positions.
+		// https://gtfs.org/realtime/reference/#message-tripdescriptor
+		// todo: add start_time
+
+		// todo: add trip.direction_id?
+
+		// fill VehicleDescriptor.id using nyctTripDescriptor.train_id
+		if (!vehiclePosition.vehicle?.id && nyctTripDescriptor?.train_id) {
+			if (!vehiclePosition.vehicle) {
+				vehiclePosition.vehicle = {}
+			}
+			vehiclePosition.vehicle.id = nyctTripDescriptor?.train_id
+		}
+	}
+
+	return {
+		matchVehiclePosition,
+	}
+}
+
+export {
+	createMatchVehiclePosition,
+}

test/01-match.js
@@ -4,6 +4,7 @@ import cloneDeep from 'lodash/cloneDeep.js'
 import {
 	stopMatching,
 	matchTripUpdate,
+	matchVehiclePosition,
 } from '../index.js'
 import { deepStrictEqual } from 'node:assert'
 
@@ -33,6 +34,22 @@ const tripUpdate072350_1_N03R = {
 			'.nyct_stop_time_update': { scheduled_track: '4' },
 		},
 	],
+}
+
+const vehiclePosition075150_1_S03R = {
+	trip: {
+		trip_id: '075150_1..S03R',
+		start_date: '20240320',
+		route_id: '1',
+		'.nyct_trip_descriptor': {
+			train_id: '01 1231+ 242/SFT',
+			is_assigned: true,
+			direction: 3
+		},
+	},
+	current_stop_sequence: 17,
+	timestamp: 1710953964n,
+	stop_id: '119S',
 }
 
 after(async () => {
@@ -89,5 +106,30 @@ test('matching an N03R TripUpdate works', async (t) => {
 			},
 		],
 		delay: 319,
+	})
+})
+
+test('matching a S03R VehiclePosition works', async (t) => {
+	const vehiclePosition = cloneDeep(vehiclePosition075150_1_S03R)
+	await matchVehiclePosition(vehiclePosition)
+
+	deepStrictEqual(vehiclePosition, {
+		trip: {
+			trip_id: 'AFA23GEN-1092-Weekday-00_075150_1..S03R',
+			start_date: '20240320',
+			route_id: '1',
+			'.nyct_trip_descriptor': {
+				train_id: '01 1231+ 242/SFT',
+				is_assigned: true,
+				direction: 3
+			},
+			schedule_relationship: 0,
+		},
+		vehicle: {
+			id: '01 1231+ 242/SFT',
+		},
+		current_stop_sequence: 17,
+		timestamp: 1710953964n,
+		stop_id: '119S',
 	})
 })

commit e58f8689942322ce66f5aea3e687c31b90a0d841

queryScheduleStopTimes(): fix exact query's name 🐛
The bug was introduced in 26d4263.

lib/query-schedule-stop-times.js
@@ -63,7 +63,7 @@ const queryScheduleStopTimes = async (cfg) => {
 // 		const t0 = performance.now()
 // 		const {rows: scheduleStopTimes} = await db.query({
 // 			// allow `pg` to create a prepared statement
-// 			name: `stop_times_suffix${stop_id === null ? '' : '_stop_id'}`,
+// 			name: `stop_times_exact${stop_id === null ? '' : '_stop_id'}`,
 // 			text: `\
 // 				SELECT
 // 					trip_id,