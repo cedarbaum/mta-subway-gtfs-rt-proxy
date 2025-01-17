4390455eee2ffda4eb4df10b6b534c27ec18661b
Jannis R <mail@jannisr.de>
2025-01-17 16:28:52 +0100

    add schedule_feed_import_attempted_timestamp_seconds metric ✅

--- test/02-service.js
+++ test/02-service.js
@@ -147,6 +147,7 @@ const purgeTestDbs = async () => {
 const debugLogMatchingMetrics = (metrics) => {
 	logger.debug({
 		schedule_feed_imported_timestamp_seconds: metrics.schedule_feed_imported_timestamp_seconds.data,
+		schedule_feed_import_attempted_timestamp_seconds: metrics.schedule_feed_import_attempted_timestamp_seconds.data,
 		tripupdates_matching_successes_total: metrics.tripupdates_matching_successes_total.data,
 		tripupdates_matching_failures_total: metrics.tripupdates_matching_failures_total.data,
 		vehiclepositions_matching_successes_total: metrics.vehiclepositions_matching_successes_total.data,
@@ -398,7 +399,7 @@ test('importing Schedule feed, matching & serving Realtime feed works', async (t
 		setScheduleFeed(BAR_FEED)
 		scheduleFeedDigest = BAR_FEED_DIGEST
 		// todo: trigger & get notified about schedule re-import instead of waiting
-		await new Promise(r => setTimeout(r, 6_000 + 3_000)) // wait for Schedule feed to be (re-)imported
+		await new Promise(r => setTimeout(r, 6_000 + 3_000)) // wait for Schedule feed to be attempted to imported
 		{
 			const importedScheduleFeeds = await fetchImportedScheduleFeeds({port})
 			strictEqual(importedScheduleFeeds.length, 2, 'should be exactly 2 imported Schedule feeds')
@@ -469,6 +470,8 @@ test('importing Schedule feed, matching & serving Realtime feed works', async (t
 
 			const scheduleFeedImported = metrics.schedule_feed_imported_timestamp_seconds.data
 			.find(({labels: l}) => l.feed_name === scheduleFeedName)
+			const scheduleFeedImportAttempted = metrics.schedule_feed_import_attempted_timestamp_seconds.data
+			.find(({labels: l}) => l.feed_name === scheduleFeedName)
 			// not imported again because the Schedule feed's digest hasn't changed
 			ok(
 				scheduleFeedImported?.value >= Math.round(tBeforeImportBar / 1000),
@@ -477,6 +480,10 @@ test('importing Schedule feed, matching & serving Realtime feed works', async (t
 			ok(
 				scheduleFeedImported?.value < Math.round(tModifyRealtime / 1000),
 				'schedule_feed_imported_timestamp_seconds should be smaller than tModifyRealtime',
+			)
+			ok(
+				scheduleFeedImportAttempted?.value >= Math.round(tModifyRealtime / 1000),
+				'schedule_feed_imported_timestamp_seconds should be larger than tModifyRealtime',
 			)
 
 			checkTripUpdatesMatchingSuccessesAndFailures(metrics, 'trip_by_suffix_stop_id')
